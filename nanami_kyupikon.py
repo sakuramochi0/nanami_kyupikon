35e4c4efedbe77e4771ec5a495b3dd3b463d8d20
sakuramochi <sakuramochi.mochi.0@gmail.com>
2015-12-06 19:25:42 +0900

    Create wrapper function to tweet. Add reply feature.

--- nanami_kyupikon.py
+++ nanami_kyupikon.py
@@ -4,10 +4,9 @@ import random
 import yaml
 import argparse
 import tweepy
-from twython import Twython, TwythonError
 
 def get_api():
-    '''Twythonオブジェクトを作る'''
+    '''TweepyのREST APIオブジェクトを作る'''
     secrets_filename = '.oauth_secrets.yaml'
     with open(secrets_filename) as f:
         secrets = yaml.load(f)
@@ -47,12 +46,17 @@ class StreamListener(tweepy.StreamListener):
             # unfollow if 'フォロー解除' in text
             if 'フォロー解除' in status.text:
                 api.destroy_friendship(screen_name=status.author.screen_name)
-                reply_tweet(status.author,screen_name, '今までありがとう♥ またね、ばいばい。')
+                tweet('今までありがとう♥ またね、ばいばい。', status.author.screen_name)
                 
             # refollow if 'フォロー' in text
             elif 'フォロー' in status.text:
                 api.create_friendship(screen_name=status.author.screen_name)
-                reply_tweet(status.author.screen_name, 'よろしくね♥')
+                tweet('よろしくね♥', status.author.screen_name)
+
+            # otherwise, reply 'きゅぴこん♥' selected at random
+            else:
+                kyupikon = get_text_kyupikon_reply()
+                tweet(kyupikon, status.author.screen_name)
         
     def on_event(self, event):
         print('! event')
@@ -68,18 +72,25 @@ class StreamListener(tweepy.StreamListener):
             # if by protected user, refollow he/r
             if event.source.get('protected'):
                 api.create_friendship(screen_name=screen_name)
-                reply_tweet(screen_name, 'フォローしてくれてありがとうキュピコン♪ フォロー解除してほしい時は、ななみに 「フォロー解除」って言ってね♥')
+                tweet('フォローしてくれてありがとうキュピコン♪ フォロー解除してほしい時は、ななみに「フォロー解除」って言ってね♥', screen_name)
 
             # otherwise, give information how to refollow
             else:
-                reply_tweet(screen_name, 'フォローしてくれてありがとうキュピコン♪ ななみにフォローしてほしい時には、「フォロー」って言ってね♥ 「フォロー解除」って言うと、フォローを解除するよ。')
+                tweet('フォローしてくれてありがとうキュピコン♪ ななみにフォローしてほしい時には、「フォロー」って言ってね♥ 「フォロー解除」って言うと、フォローを解除するよ。', screen_name)
 
     def on_error(self, error_code):
         print('error:', error_code)
 
-def reply_tweet(screen_name, status):
-    status = '@{} {}'.format(screen_name, status)
-    api.update_status(status=status)
+def tweet(status, screen_name=None):
+    '''statusで指定したテキストをツイートをする。screen_nameがあればリプライを送る'''
+    if screen_name:
+        status = '@{} {}'.format(screen_name, status)
+    try:
+        api.update_status(status=status)
+        print('--')
+        print('Tweeted: \'{}\''.format(status))
+    except tweepy.TweepError as e:
+        print(e, file=sys.stderr)
 
 def make_text_kyupikons():
     '''ななみがきゅぴこんするbot(@nanami_kyupikon) 由来の30種類+αの「きゅぴこん」を作成する'''
@@ -90,31 +101,17 @@ def make_text_kyupikons():
     random.shuffle(kyupikons)
     return kyupikons
 
-def tweet_kyupikon(action='random_choice'):
-    if action == 'random_choice':
-        status = get_text_kyupikon()
-    print('--')
-    print('Tweeting: \'{}\''.format(status))
-    try:
-        api.update_status(status=status)
-    except TwythonError as e:
-        print(e, file=sys.stderr)
-
-def tweet_kyupikon(action='random_choice'):
-    if action == 'random_choice':
-        status = get_text_kyupikon()
-    print('--')
-    print('Tweeting: \'{}\''.format(status))
-    try:
-        api.update_status(status=status)
-    except tweepy.TweepError as e:
-        print(e, file=sys.stderr)
+def tweet_kyupikon():
+    '''ランダムに選ばれた「きゅぴこん♥」のキューから一つ取り出してツイートする'''
+    status = get_text_kyupikon()
+    tweet(status)
         
 def get_tweets_text_list():
+    '''デバッグ用: 最新100個のツイートテキストのリストを取得する'''
     return [tw.text for tw in api.user_timeline(count=100)]
 
 def get_text_kyupikon():
-    '''「きゅぴこん」のキューから一つ取り出してテキストを返す'''
+    '''「きゅぴこん♥」のキューから一つ取り出してテキストを返す'''
     global text_kyupikons_queue
     if not text_kyupikons_queue:
         text_kyupikons_queue = make_text_kyupikons()
@@ -122,6 +119,18 @@ def get_text_kyupikon():
 
     # update queue
     save_yaml('text_kyupikons_queue.yaml', text_kyupikons_queue)
+
+    return kyupikon
+    
+def get_text_kyupikon_reply():
+    '''リプライ用の「きゅぴこん♥」のキューから一つ取り出してテキストを返す'''
+    global text_kyupikons_reply_queue
+    if not text_kyupikons_reply_queue:
+        text_kyupikons_reply_queue = make_text_kyupikons()
+    kyupikon = text_kyupikons_reply_queue.pop()
+
+    # update queue
+    save_yaml('text_kyupikons_reply_queue.yaml', text_kyupikons_reply_queue)
 
     return kyupikon
     
